8a159397376a951de8cf8603abe2cea9306c33b9
Marcelo_Lima <marcelolima152021@gmail.com>
2022-07-01 13:59:37 -0300

    Projeto criado

---
+++ userdept/src/test/java/com/marcelo/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
